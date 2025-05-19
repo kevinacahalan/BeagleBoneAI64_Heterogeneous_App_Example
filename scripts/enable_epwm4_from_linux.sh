ec7250b22b37e1e35be5693e0128ae00a4ad13f9
Kevin Cahalan <69361467+kevinacahalan@users.noreply.github.com>
2025-05-19 13:15:51 -0400

    Update enable_epwm4_from_linux.sh

--- scripts/enable_epwm4_from_linux.sh
+++ scripts/enable_epwm4_from_linux.sh
@@ -4,5 +4,5 @@ sudo beagle-pwm-export --pin p9_25
 echo 0 > /dev/beagle/pwm/P9_25/duty_cycle
 echo 0 > /dev/beagle/pwm/P9_25/period
 
-# The R5 firmware will crash if this is from not done from linux..some kinda of bug
+# The R5 firmware will crash if this is first not done from linux..some kinda of bug
 echo 1 > /dev/beagle/pwm/P9_25/enable
